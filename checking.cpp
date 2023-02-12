184d5961857e5a93856ac101da0adb2dea0badbb
akashtripathi0109 <57222581+akashtripathi0109@users.noreply.github.com>
2023-02-12 21:16:32 +0530

    Initial Commit

---
+++ checking.cpp
@@ -0,0 +1,26 @@
+wscfgyhwejcfgb
+dfjer
+dljer
+jkdvbgk
+eljn kljvn 
+jvnefkvjgun
+lkvngj
+v int main(int argc, char const *argv[])
+{
+    vkl nbkdfmn
+
+    v lkn
+    fv kml
+    flvmk fklv
+    {
+        int 
+        lkcvnsdv
+        int 7jc
+
+rfgvnjk
+vsdljn vjk
+alignas    }
+    return v l;
+    ds;kmvlsdkfv
+    edvkdns
+}
