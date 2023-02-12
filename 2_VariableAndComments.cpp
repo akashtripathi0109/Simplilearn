119d6e764d4dbda375cf284534fdd2b7cf48729d
akashtripathi0109 <57222581+akashtripathi0109@users.noreply.github.com>
2023-02-12 21:11:59 +0530

    first commit

---
+++ 2_VariableAndComments.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+// this is a multi line comment
+/* this
+is 
+a 
+multi
+line 
+comment */
+int main(){
+    int sum=6;
+    cout<< "helloworld"<< sum;
+    return 0;
+}
---
+++ 4_BasicInputOutput.cpp
@@ -0,0 +1,13 @@
+    #include<iostream>
+    using namespace std;
+    int main()
+    {
+        int num1, num2;
+        cout<<"Enter the value of num1: "; //"<<" is called Insertion Operator
+        cin>>num1;//">>" is called Extraction Operator
+        cout<<"Enter the value of num2: ";
+        cin>>num2;
+        cout<<"Sum of num1 and num2 is: "<<num1+num2;
+        return 0;
+    }
+    
